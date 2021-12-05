c028e243ef01bd5fa48db61104dc7c2d3992251c
sivasankari-vk <j.sivasankari@gmail.com>
2021-12-05 22:44:23 +0000

    Work of Week5:day1

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+      User.find_by(id: session[:user_id])
+    end
+end
+
 # When a browser requests the root of the application
 get '/' do
 
@@ -31,12 +37,47 @@ post '/signup' do
     # if all user params are present, user validations will pass and user can be saved
     if @user.save
 
-        "User #{username} saved successfully!"
+        redirect to('/login')
 
     else
 
         erb(:signup)
 
     end
+
+end
+
+get '/login' do
+    erb(:login)
+end
+
+post '/login' do
+
+    username = params[:username]
+    password = params[:password]
+
+    # 1. Find user by username
+    @user = User.find_by(username: username)
+
+    #2. If that user exists
+    #check if that user's password matches the password input
+    #3. if the passwords match
+
+    if @user && @user.password == password
+        #login successfull
+        session[:user_id] = @user.id
+        "Success! User with id #{session[:user_id]} is logged in!!"
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end
+
+end
+
+get '/logout' do
+
+    session[:user_id] = nil
+    redirect to('/')
 
 end
